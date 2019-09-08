0cd3256a82e7974d38fffbe6bec2ab6c6cb1d519
Ritik Raina <rraina@ucsd.edu>
2019-09-09 00:26:45 +0700

    Update video_facial_landmarks.py

--- video_facial_landmarks.py
+++ video_facial_landmarks.py
@@ -1,8 +1,4 @@
-# USAGE
-# python video_facial_landmarks.py --shape-predictor shape_predictor_68_face_landmarks.dat
-# python video_facial_landmarks.py --shape-predictor shape_predictor_68_face_landmarks.dat --picamera 1
-
-# import the necessary packages
+# Imports
 from imutils.video import VideoStream
 from imutils import face_utils
 import datetime
@@ -12,58 +8,46 @@ import time
 import dlib
 import cv2
  
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-p", "--shape-predictor", required=True,
-	help="path to facial landmark predictor")
-ap.add_argument("-r", "--picamera", type=int, default=-1,
-	help="whether or not the Raspberry Pi camera should be used")
-args = vars(ap.parse_args())
- 
-# initialize dlib's face detector (HOG-based) and then create
-# the facial landmark predictor
-print("[INFO] loading facial landmark predictor...")
+# Initializing dlib's face detector (HOG-based)
+# Creating the facial shape predictor using the 'shape_predictor_81_face_landmarks.dat' file
+print("Initializing Facial Landmarking Predictor ->")
+#Detecting the frontal faces
 detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor(args["shape_predictor"])
+predictor = dlib.shape_predictor('shape_predictor_81_face_landmarks.dat')
 
-# initialize the video stream and allow the cammera sensor to warmup
-print("[INFO] camera sensor warming up...")
-vs = VideoStream(usePiCamera=args["picamera"] > 0).start()
+# Initializing the camera sensor to warm up
+print("Camera Sensor Warming Up ->")
+vs = VideoStream(0).start()
 time.sleep(2.0)
 
-# loop over the frames from the video stream
+# Looping over all the frames from the webcam stream
 while True:
-	# grab the frame from the threaded video stream, resize it to
-	# have a maximum width of 400 pixels, and convert it to
-	# grayscale
+    # Grabbing the frames
 	frame = vs.read()
+    # Setting the frame size to 500
 	frame = imutils.resize(frame, width=500)
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-	# detect faces in the grayscale frame
-	rects = detector(gray, 0)
+	# Detecting the faces from the frame
+	rects = detector(frame, 0)
 
-	# loop over the face detections
+	# Looping throughe each face detection
 	for rect in rects:
-		# determine the facial landmarks for the face region, then
-		# convert the facial landmark (x, y)-coordinates to a NumPy
-		# array
-		shape = predictor(gray, rect)
+		shape = predictor(frame, rect)
 		shape = face_utils.shape_to_np(shape)
 
-		# loop over the (x, y)-coordinates for the facial landmarks
-		# and draw them on the image
+		# Looping over the facial lanmarks based on their (x,y) coordinates
+        # Drawing a red circle over those landmarks
 		for (x, y) in shape:
 			cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
 	  
-	# show the frame
+	# Showing the frame
 	cv2.imshow("Frame", frame)
 	key = cv2.waitKey(1) & 0xFF
  
-	# if the `q` key was pressed, break from the loop
+	# Break from loop/end when the 'q' is pressed
 	if key == ord("q"):
 		break
- 
-# do a bit of cleanup
+        
+# Destroying the windows once the frame display is over
 cv2.destroyAllWindows()
 vs.stop()
