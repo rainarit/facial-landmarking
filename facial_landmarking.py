fb84c4d49a8c666e8f0aaefc5265c540b2644911
Ritik Raina <rraina@ucsd.edu>
2019-09-09 10:41:27 +0700

    Update facial_landmarking.py

--- facial_landmarking.py
+++ facial_landmarking.py
@@ -2,11 +2,32 @@
 from imutils.video import VideoStream
 from imutils import face_utils
 import datetime
-import argparse
 import imutils
 import time
 import dlib
 import cv2
+
+# import the necessary packages
+from scipy.spatial import distance as dist
+
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+def eye_aspect_ratio(eye):
+	# compute the euclidean distances between the two sets of
+	# vertical eye landmarks (x, y)-coordinates
+	A = dist.euclidean(eye[1], eye[5])
+	B = dist.euclidean(eye[2], eye[4])
+
+	# compute the euclidean distance between the horizontal
+	# eye landmark (x, y)-coordinates
+	C = dist.euclidean(eye[0], eye[3])
+
+	# compute the eye aspect ratio
+	ear = (A + B) / (2.0 * C)
+
+	# return the eye aspect ratio
+	return ear
  
 # Initializing dlib's face detector (HOG-based)
 # Creating the facial shape predictor using the 'shape_predictor_68_face_landmarks.dat' file
@@ -21,6 +42,7 @@ vs = VideoStream(0).start()
 time.sleep(2.0)
 
 blink_count = 0
+total = 0
 
 # Looping over all the frames from the webcam stream
 while True:
@@ -28,38 +50,51 @@ while True:
 	frame = vs.read()
     # Setting the frame size to 500
 	frame = imutils.resize(frame, width=500)
-	cv2.putText(frame, "Blink Count = " + str(blink_count), (0, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	cv2.putText(frame, "Blink Count = " + str(total), (0, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
 
 	# Detecting the faces from the frame
 	rects = detector(frame, 0)
+    
 
 	# Looping through each face detection
 	for rect in rects:
-		shape = predictor(frame, rect)
+		shape = predictor(gray, rect)
 		shape = face_utils.shape_to_np(shape)
-
+        
+		leftEye = shape[lStart:lEnd]
+		rightEye = shape[rStart:rEnd]
+		leftEAR = eye_aspect_ratio(leftEye)
+		rightEAR = eye_aspect_ratio(rightEye)
+ 
+		# average the eye aspect ratio together for both eyes
+		ear = (leftEAR + rightEAR) / 2.0
+        
+		leftEyeHull = cv2.convexHull(leftEye)
+		rightEyeHull = cv2.convexHull(rightEye)
+		cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+		cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+        
+		if ear < 0.25:
+			blink_count += 1
+ 
+		# otherwise, the eye aspect ratio is not below the blink
+		# threshold
+		else:
+			# if the eyes were closed for a sufficient number of
+			# then increment the total number of blinks
+			if blink_count >= 3:
+				total += 1
+ 
+			# reset the eye frame counter
+			blink_count = 0
+            
 		# Looping over the facial landmarks based on their (x,y) coordinates
         # Drawing a red circle over those landmarks
-		count = 0
-		top_right_1 = 0
-		bottom_right_1 = 0
-		top_right_2 = 0
-		bottom_right_2 = 0
 		for (x, y) in shape:
-			print(float(x))           
-			count = count + 1
-			cv2.circle(frame, (x, y), 0, (0, 0, 255), -1)
-			if (count == 38):
-				top_right_1 = y
-			if (count == 42):
-				bottom_right_1 = y
-			if (count == 39):
-				top_right_2 = y
-			if (count == 41):
-				bottom_right_2 = y
-		if (bottom_right_1 - top_right_1 < 3 and bottom_right_2 - top_right_2 < 3):
-			blink_count = blink_count + 1
-	  
+			cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
+                
+                
 	# Showing the frame
 	cv2.imshow("Frame", frame)
 	key = cv2.waitKey(1) & 0xFF
